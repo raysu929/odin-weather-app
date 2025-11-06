9f504fbb4ac876e69c1b7241067b38765fb531da
Raeesah <raeesahmoola529@gmail.com>
2025-11-06 17:01:24 +0200

    Add module rules for html, css and images. Create html and css files.

--- webpack.common.js
+++ webpack.common.js
@@ -8,8 +8,25 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: "Production",
+      template: "./src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
